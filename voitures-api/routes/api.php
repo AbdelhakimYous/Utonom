6344085097e240b09a06d1056e76de95364caead
Abdelhakim Yous <MrYousAbdelhakim@gmail.com>
2025-07-21 10:05:56 -0400

    fixing

--- voitures-api/routes/api.php
+++ voitures-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CreerVoitureController;
 
@@ -8,3 +9,19 @@ Route::post('/creervoiture', [CreerVoitureController::class, 'createVoiture']);
 use App\Http\Controllers\ChercherVoitureController;
 
 Route::post('/voiture-plus-proche', [ChercherVoitureController::class, 'voitureLaPlusProche']);
+
+Route::get('/testDb', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json([
+            'success' => true,
+            'message' => 'Connexion à la base de données réussie 🚀'
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Échec de la connexion à la base de données ❌',
+            'error' => $e->getMessage()
+        ], 500);
+    }
+});
